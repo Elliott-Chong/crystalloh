a97bc6e71032cf75f9070f47b2fafbeb38cf3371
Elliott Chong <77007117+Elliott-Chong@users.noreply.github.com>
2025-01-29 22:49:42 +0800

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  rewrites: async () => {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://thankful-volunteers-703936.framer.app/:path*'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
